4f5ad721ddaa29101736e746f75d8438e8357db2
zakariae-belkassem <Zakaria.Belkassem@emsi-edu.ma>
2024-04-06 21:20:43 +0000

    Initial commit

---
+++ src/main/java/com/example/bloperso/BloPersoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloperso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloPersoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloPersoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bloperso/BloPersoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloperso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloPersoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
