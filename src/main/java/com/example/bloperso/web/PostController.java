5fbc7fea153bb5c6c8b36bfb3f92c0cf21becbc0
zakariae-belkassem <Zakaria.Belkassem@emsi-edu.ma>
2024-04-08 14:57:56 +0000

    added a controller for sepecefic post

--- src/main/java/com/example/bloperso/web/PostController.java
+++ src/main/java/com/example/bloperso/web/PostController.java
@@ -1,13 +1,14 @@
 package com.example.bloperso.web;
 
 import com.example.bloperso.Entities.Post;
+import com.example.bloperso.Entities.PostCategorie;
 import com.example.bloperso.Entities.Visibilite;
 import com.example.bloperso.dao.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,10 +17,18 @@ public class PostController {
     @Autowired
     private PostRepository postRepository ;
 
-    @RequestMapping(name = "")
+    @RequestMapping(value = "")
     public String index(Model model){
         List<Post> posts = postRepository.findAll().stream().filter(e->e.getVisibilite()==Visibilite.Public).toList();
         model.addAttribute("poste",posts);
         return "index";
     }
+    @GetMapping (value = "/postN")
+    public String SpePost(Model model, @RequestParam(name = "id",defaultValue = "0") Long id){
+        Post post = postRepository.findById(id).orElse(null);
+        model.addAttribute("poste",post);
+        return "postN";
+    }
+
+
 }
