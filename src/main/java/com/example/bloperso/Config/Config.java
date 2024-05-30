070665d92d1d839e72a3732ac310690932f5d8dd
omar hafidi alaoui <ohafidi.oh@gmail.com>
2024-05-30 23:28:03 +0100

    fixed csrf disabling challenges

--- src/main/java/com/example/bloperso/Config/Config.java
+++ src/main/java/com/example/bloperso/Config/Config.java
@@ -11,11 +11,12 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.CompositeLogoutHandler;
+
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.util.AntPathMatcher;
 
@@ -65,7 +66,7 @@ public class Config {
                                 .permitAll()
                 )
                 .logout(Customizer.withDefaults())
-
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
     @Bean
